c5c426c0a6dd4741e640a8aef62c477d508b55a3
andrey Nemodruk <nemodruk.briz.ua>
2021-11-03 09:35:24 +0200

    add eslint

--- index.js
+++ index.js
@@ -1,76 +1,78 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
-const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
-const ffmpeg = require("fluent-ffmpeg");
-ffmpeg.setFfmpegPath(ffmpegPath);
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+
 const port = 3000;
-const allowedTypes = ["video/mp4", "video/quicktime", "video/x-msvideo"];
+const allowedTypes = ['video/mp4', 'video/quicktime', 'video/x-msvideo'];
 const ext = {
-  "video/mp4": "mp4",
-  "video/quicktime": "mov",
-  "video/x-msvideo": "avi",
+  'video/mp4': 'mp4',
+  'video/quicktime': 'mov',
+  'video/x-msvideo': 'avi',
 };
+
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 const server = http.createServer(async (req, res) => {
   const urlPath = url.parse(req.url, true).pathname;
-  if (req.method !== "POST") {
+  if (req.method !== 'POST') {
     res.statusCode = 404;
-    res.end("Неверный метод запроса");
+    res.end('Неверный метод запроса');
     return;
   }
   switch (urlPath) {
-    case "/upload":
-      if (!allowedTypes.includes(req.headers["content-type"])) {
-        res.writeHead(422, { "Content-Type": "text/plain" });
+    case '/upload':
+      if (!allowedTypes.includes(req.headers['content-type'])) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
         res.end(`status code=${res.statusCode}`);
         return;
       }
-      if (req.headers["content-type"] === "video/mp4") {
-        let writeStream = fs.createWriteStream(
-          `output/${Date.now()}${uuidv4()}.mp4`
+      if (req.headers['content-type'] === 'video/mp4') {
+        const writeStream = fs.createWriteStream(
+          `output/${Date.now()}${uuidv4()}.mp4`,
         );
         req.pipe(writeStream);
-        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(`status code=${res.statusCode}`);
-        writeStream.on("error", function (err) {
+        writeStream.on('error', (err) => {
           console.log(err);
         });
       } else {
-        let writeStream = fs.createWriteStream(
-          `temp/temp.${ext[req.headers["content-type"]]}`
+        const writeStream = fs.createWriteStream(
+          `temp/temp.${ext[req.headers['content-type']]}`,
         );
         req.pipe(writeStream);
-        ffmpeg(`temp/temp.${ext[req.headers["content-type"]]}`)
-          .videoCodec("libx264")
-          .audioCodec("libmp3lame")
-          .size("320x240")
-          .on("error", function (err) {
-            console.log("An error occurred: " + err.message);
+        ffmpeg(`temp/temp.${ext[req.headers['content-type']]}`)
+          .videoCodec('libx264')
+          .audioCodec('libmp3lame')
+          .size('320x240')
+          .on('error', (err) => {
+            console.log(`An error occurred: ${err.message}`);
           })
-          .on("end", function () {
+          .on('end', () => {
             fs.unlinkSync(
-              `temp/temp.${ext[req.headers["content-type"]]}`,
-              function (err) {
+              `temp/temp.${ext[req.headers['content-type']]}`,
+              (err) => {
                 if (err) return console.log(err);
-                console.log("file deleted successfully");
-              }
+                console.log('file deleted successfully');
+                return false;
+              },
             );
-            console.log("Processing finished !");
+            console.log('Processing finished !');
           })
           .save(`output/${Date.now()}${uuidv4()}.mp4`);
-        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(`status code=${res.statusCode}`);
-        writeStream.on("error", function (err) {
+        writeStream.on('error', (err) => {
           console.log(err);
         });
       }
       break;
     default:
       res.statusCode = 404;
-      res.end("Проверьте адрес");
+      res.end('Проверьте адрес');
   }
 });
 
