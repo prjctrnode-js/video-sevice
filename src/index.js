77260b096e40317820ac0c95d5c59be333eb5885
andrey Nemodruk <nemodruk.briz.ua>
2021-12-06 09:14:58 +0200

    add validatorMiddleware

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ require('dotenv-defaults').config();
 const errorHandler = require('./middlewares/errorHandler');
 const loggerMiddleware = require('./middlewares/loggerMiddleware');
 const logger = require('./helpers/logger');
+const validatorMiddleware = require('./middlewares/validatorMiddleware');
 const addRoutes = require('./middlewares/routes');
 
 const app = new Koa();
 app.use(loggerMiddleware);
 app.use(bodyparser());
 app.use(errorHandler());
+app.use(validatorMiddleware);
 app.use(addRoutes());
 
 app.listen(process.env.PORT, () => {
